36a91a9af9f8beb2e13330b2efc44b606ffc08af
mark-todd <markpeter.todd@hotmail.co.uk>
2023-06-08 11:55:55 +0100

    Generalised and added pre and post key

--- bank_simulator/banksim.cc
+++ bank_simulator/banksim.cc
@@ -8,12 +8,18 @@ class Interface {
   public:
     virtual int on_key(int &ch) = 0;
 
+    virtual void pre_key() {};
+
+    virtual void post_key() {};
+
     int operator()() {
         int ch = KEY_HOME;
         do {
             clear();
+            pre_key();
             int out = on_key(ch);
-            if (out == -1) {
+            post_key();
+            if (out == -2) {
                 continue;
             } else {
                 return out;
@@ -24,21 +30,23 @@ class Interface {
     }
 };
 
-class SelectInterface : public Interface {
+class VerticalInterface : public Interface {
   private:
     int selectedOption;
     int totalOptions;
     std::vector<std::string> stringOptions;
     bool num_mode;
+    std::string prefix;
 
   public:
+
     int on_key(int &ch) override {
         int numOptions = num_mode ? totalOptions : stringOptions.size();
-        
+
         switch (ch) {
         case KEY_UP:
             selectedOption =
-                (selectedOption > 0) ? selectedOption - 1 : numOptions-1;
+                (selectedOption > 0) ? selectedOption - 1 : numOptions - 1;
             break;
         case KEY_DOWN:
             selectedOption =
@@ -46,32 +54,52 @@ class SelectInterface : public Interface {
             break;
         }
         // Display options
-        printw("Please select an account:\n");
+        
         for (int i = 0; i < numOptions; i++) {
             std::string str = i == selectedOption ? "-> " : "   ";
             if (num_mode) {
-                printw((str + "Account %d\n").c_str(), i + 1);
+                printw((str + prefix + " %d\n").c_str(), i + 1);
             } else {
                 printw((str + stringOptions[i] + "\n").c_str(), i);
             }
-            
         }
         switch (ch) {
         case 10: // Enter key
             return selectedOption;
         case 'q':
-            return 0; // quit
-        default:      // continue loop
-            return -1;
+            return -1; // quit
+        default:       // continue loop
+            return -2;
         }
     }
 
-    SelectInterface(int options, int selectedOption = 1)
-        : selectedOption(selectedOption), totalOptions(options), num_mode(true) {}
+    VerticalInterface(int options, int selectedOption = 0, std::string prefix = "Option")
+        : selectedOption(selectedOption), totalOptions(options), prefix(prefix),
+          num_mode(true) {}
 
-    SelectInterface(std::vector<std::string> options, int selectedOption = 1)
-        : selectedOption(selectedOption), stringOptions(options), num_mode(false) {}
+    VerticalInterface(std::vector<std::string> options, int selectedOption = 0, std::string prefix = "Option")
+        : selectedOption(selectedOption), stringOptions(options), prefix(prefix),
+          num_mode(false) {}
+};
 
+class AccountSelectInterface : public VerticalInterface {
+    public:
+        void pre_key() override {
+            printw("Please select an account:\n");
+        }
+    AccountSelectInterface(int accounts, int selectedAccount = 0)
+        : VerticalInterface(accounts, selectedAccount, "Account") {}
+};
+
+class LoginSelectInterface : public VerticalInterface {
+  private:
+    std::string account_text;
+  public:
+    void pre_key() override { printw(("Welcome to account: " + account_text + "\n").c_str()); }
+
+    LoginSelectInterface(std::vector<std::string> options, std::string account_text = "",
+                         int selectedOption = 0)
+        : VerticalInterface(options, selectedOption) {}
 };
 
 int main() {
@@ -79,13 +107,16 @@ int main() {
     cbreak();             // Line buffering disabled
     noecho();             // Don't display keypresses
     keypad(stdscr, TRUE); // Enable function keys
-    std::vector<std::string> options = {"Opt1", "Opt2"};
-    SelectInterface account_select(3);
-    int out = account_select(); // Call the functor like a function
-    if (out == 0) {
-        goto Terminate;
+    AccountSelectInterface account_select(3);
+
+    int account_num = account_select(); // Call the functor like a function
+    if (account_num == -1) {
+        endwin();
+        return 0;
     }
-Terminate:
+    LoginSelectInterface login({"Check balance", "Withdraw", "Deposit"}, std::to_string(account_num));
+    int login_selection = login();
+
     endwin();
     return 0;
 }
